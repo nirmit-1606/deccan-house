47efcf43670e0e39ef7effc27d6f0eb7d5c4755a
Nirmit P <nirmitpatel16620@gmail.com>
2025-12-01 12:26:09 -0500

    Refactor menu data handling; integrate Notion API for dynamic menu items

---
+++ src/_data/menu.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { Client } = require("@notionhq/client");
+
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
+
+module.exports = async function() {
+  const databaseId = process.env.MENU_DATABASE_ID;
+
+  const response = await notion.databases.query({
+    database_id: databaseId
+  });
+
+  return response.results.map(item => {
+    return {
+      id: item.id,
+      name: item.properties.Name.title[0]?.plain_text,
+      price: item.properties.Price.number,
+      category: item.properties.Category.select?.name,
+      description: item.properties.Description.rich_text[0]?.plain_text
+    };
+  });
+};
